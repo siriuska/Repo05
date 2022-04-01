656c3418df0d86f2f094668b435307b0ca8e5bb8
siriuska <you@example.com>
2022-04-01 12:57:49 +0200

    Declare string variables

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+function main() {
+  // only change code below this line
+  var myFirstName = 'Aida';
+  var myLastName = 'Ljajic';
+  var myHomeTown = 'Sarajevo';
+  // only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;

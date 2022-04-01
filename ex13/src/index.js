c68e851268a683de7f053f0e63612f88008c16b5
siriuska <you@example.com>
2022-04-01 16:00:21 +0200

    Bracket notatation, last character

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,8 +1,8 @@
 function main () {
-// only change code below this line
+// only change code below this code
 var lastName = "Tesla";
 var lastLetterOfLastName = lastName[lastName.length - 1];
-// only change code above this line
+// only change code above this code
 return lastLetterOfLastName;
 }
 

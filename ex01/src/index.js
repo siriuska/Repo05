7f48bc35b962a438f042ce32271f2b776925234c
siriuska <you@example.com>
2022-04-01 13:20:43 +0200

    Declare string variables

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 function main() {
   // only change code below this line
-  var myFirstName = 'Aida';
-  var myLastName = 'Ljajic';
-  var myHomeTown = 'Sarajevo';
+  var myFirstName = "Aida";
+  var myLastName = "Ljajic";
+  var myHomeTown = "Sarajevo";
   // only change code above this line
   return {
     myFirstName,

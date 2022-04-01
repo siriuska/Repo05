046f67976bb4f430a08709176918565b1d2d052d
siriuska <you@example.com>
2022-04-01 17:15:10 +0200

    World Blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+// only change code below this code
+var myNoun = "dog";
+var myVerb = "ran";
+var myAdjective = "big";
+var myAdverb = "quickly";
+
+var wordBlanks = "Once my " + myNoun + " had " + myVerb + " away, because he got scared of a " + myAdjective + " spider, but he came back " + myAdverb + " enough."
+// only change code above this code
+return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;

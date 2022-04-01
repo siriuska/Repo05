d74c0b8c0b0d00ebeef4a71071c591e8e04b1f38
siriuska <you@example.com>
2022-04-01 20:32:16 +0200

    Constructing strings with variables

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,7 +1,7 @@
 function main() {
     // only change code below this line
     var myName = "Sirius";
-    var myString = "My name is " + myName + " and I am well!"
+    var myString = "My name is " + myName + " and I am well!";
     // only change code above this line
     return myString;
     }
